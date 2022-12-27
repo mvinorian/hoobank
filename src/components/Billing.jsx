cf1dd99a9c2369a4751c3240041bc15144fa2550
mvinorian <vinorianmuhammad@gmail.com>
2022-12-28 01:54:13 +0700

    feat: add billing and card deal sections

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../styles";
 
 const Billing = () => {
-  return <div>Billing</div>;
+  return (
+    <section id="product" className={layout.sectionReverse}>
+      <div className={layout.sectionImgReverse}>
+        <img src={bill} alt="" className="w-full z-[5] relative" />
+
+        <div className="absolute z-[3] -left-1/2 top-0 w-1/2 h-1/2 rounded-full white-gradient"></div>
+        <div className="absolute z-[0] -left-1/2 bottom-0 w-1/2 h-1/2 rounded-full pink-gradient"></div>
+      </div>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="sm:block hidden" />
+          billing &amp; invoicing.
+        </h2>
+
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+
+        <div className="flex flex-wrap sm:mt-10 mt-6">
+          <img
+            src={apple}
+            alt=""
+            className="w-[128px] h-[128px] object-contain mr-5 cursor-pointer"
+          />
+
+          <img
+            src={google}
+            alt=""
+            className="w-[128px] h-[128px] object-contain cursor-pointer"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Billing;
